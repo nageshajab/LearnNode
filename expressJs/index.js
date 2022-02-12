e2b036b25a43cec7afe79959f5d66466c66aac9a
Nagesh Ajab <najab@halfpenny.com>
2022-02-12 20:25:19 +0530

    ejs template rendering engine basic details added like showing data , but form posting is still not done

--- expressJs/index.js
+++ expressJs/index.js
@@ -2,22 +2,37 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 //configure static folder 
-const wwwPath= path.join(__dirname,'www');
+const wwwPath = path.join(__dirname, 'www');
 app.use(express.static(wwwPath));
 
-app.get('',(req,res)=>{
+app.get('', (req, res) => {
     res.sendFile(`${wwwPath}/index.html`);
 });
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     //res.send('this is about page');sending raw test message
     res.sendFile(`${wwwPath}/about.html`);
 });
 
+app.get('/profile', (req, res) => {
+    const user = {
+        name: 'nagesh ajab',
+        email: 'ajab.nagesh@gmail.com',
+        city: 'Pune',
+        skills: ['c#', 'asp.net', 'sql server', '.net core']
+    };
+    res.render('profile', { user });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 //put this condition at bottom, when no path matches, it will return 404.html
-app.get('*',(req,res)=>{
+app.get('*', (req, res) => {
     //res.send('this is about page');sending raw test message
     res.sendFile(`${wwwPath}/404.html`);
 });
